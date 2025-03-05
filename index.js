eed0af6770a90a70f6500547cff433196a9bb725
luckyantony <luckyantonyofficial@gmail.com>
2025-03-05 21:56:10 +0300

    Javascript arithmetic operations

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random()) + 1;
+
+let num3 = 40;
+let num4 = 6;
+let mod = num3 % num4;
+
+let max = Math.max(7, 12,5,14,20);
